0a4c7bca0bffcf86f97eb607ed004c46c934a8cf
Paulooo0 <paulohenrique.n.s00@gmail.com>
2024-03-23 17:01:09 -0300

    adjusts in dependencies and padronization in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
-  return gulp.src('src/scripts/*.js')
+  return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('./dist/js'))
 }
 
 function styles() {
-  return gulp.src('src/styles/*.scss')
+  return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('./dist/css'))
 }
 
 function images() {
-  return gulp.src('src/images/*')
+  return gulp.src('./src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('./dist/images'))
 }
 
 exports.default = gulp.parallel(styles, images, scripts)
